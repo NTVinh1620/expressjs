2e03df81293bbbdfca56bee0ac9ff4d8c1981e99
NTVinh <rukitori1620@gmail.com>
2022-07-12 15:31:55 +0700

    install prettier, lint-staged, husky

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const { engine } = require('express-handlebars');
+const route = require('./routes');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
+
+// app.use(morgan('combined'));
+
+app.engine('.hbs', engine({ extname: '.hbs' }));
+app.set('view engine', '    .hbs');
+            app.set('views', './src/resources/views');
+
+route(app);
+
+app.listen(3000);
